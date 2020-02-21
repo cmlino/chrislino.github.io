3890f46adee262463fa8aae599c3fe9b55d5e045
peterg4 <43735968+peterg4@users.noreply.github.com>
2020-02-21 08:48:42 -0500

    Added pathfinder

---
+++ Pathfinder/build/precache-manifest.8ed3864372bcc2b23c1d71d735c1eabd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d0058e068cff47b912ef6d51b0b2020",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a0fee2f3fd6a5b57406",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "4c4feb1ccddcb30e0442",
+    "url": "/static/css/main.77d65d19.chunk.css"
+  },
+  {
+    "revision": "2a0fee2f3fd6a5b57406",
+    "url": "/static/js/2.5fbe8233.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.5fbe8233.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c4feb1ccddcb30e0442",
+    "url": "/static/js/main.84ae9821.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
