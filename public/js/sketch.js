556fb0f53987051896a498553d7e3870b14c79a0
peterg4 <43735968+peterg4@users.noreply.github.com>
2020-04-11 15:43:23 -0400

    growing tree working

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -1,23 +1,32 @@
 p5.disableFriendlyErrors = true; // disables FES
+
 function setup() {
-  var can = createCanvas(400, 400);
+  var can = createCanvas(600, 600);
   can.parent('sketch');
+  frameRate(30);
 }
+let depth = 100;
 function draw() {
+  
   let angle = PI/6;
   background(34);
-  translate(200,height)
-  stroke(255);
-
-  branch(67, angle, 6);
-
+  clear();
+  translate(300,height)
+  stroke(255/*,frameCount*3*/);
+  branch(100, angle, 6);
+  if(frameCount >= 20) {
+    noLoop();
+ //   saveCanvas('myCanvas', 'png');
+  }
+  depth*=.75;
 }
 function branch(length, angle, w) {
   if(w)
     strokeWeight(w);
   line(0,0,0,-length);
+  //drawLine(0,0,0,-length); //make a function to animate the line drawing process
   translate(0,-length);
-  if(length > 3) {
+  if(length > depth && length > 4) {
     if(w > 1)
       w--;
     push();
@@ -29,9 +38,5 @@ function branch(length, angle, w) {
     branch(length*.75,angle*.9,w);
 
     pop();
-  } else {
-    noLoop();
-  }
-  
-  //line(0,0,0,-length);
+  } 
 }
