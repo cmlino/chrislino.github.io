96ef424db6e32218675240d94c9785db064445ac
peterg4 <43735968+peterg4@users.noreply.github.com>
2020-04-11 11:45:24 -0400

    thickness and animated chevron

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -3,30 +3,35 @@ function setup() {
   var can = createCanvas(400, 400);
   can.parent('sketch');
 }
-let weight = 5;
 function draw() {
   let angle = PI/6;
   background(34);
   translate(200,height)
   stroke(255);
 
-  branch(67, angle);
+  branch(67, angle, 6);
 
 }
-function branch(length, angle) {
+function branch(length, angle, w) {
+  if(w)
+    strokeWeight(w);
   line(0,0,0,-length);
   translate(0,-length);
   if(length > 3) {
+    if(w > 1)
+      w--;
     push();
     rotate(angle);
-    branch(length*.75,angle*.9);
+    branch(length*.75,angle*.9,w);
     pop();
     push();
     rotate(-angle);
-    branch(length*.75,angle*.9);
+    branch(length*.75,angle*.9,w);
+
     pop();
   } else {
     noLoop();
   }
+  
   //line(0,0,0,-length);
 }
