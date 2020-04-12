e48a01f0427e9c7a11f89e90b3d7d7be97455e06
peterg4 <43735968+peterg4@users.noreply.github.com>
2020-04-12 00:31:18 -0400

    better animation on cover + favicon

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -3,7 +3,7 @@ p5.disableFriendlyErrors = true; // disables FES
 function setup() {
   var can = createCanvas(600, 600);
   can.parent('sketch');
-  frameRate(60);
+  frameRate(30);
 }
 let fc = 0;
 let depth = 100;
@@ -15,12 +15,12 @@ function draw() {
   translate(300,height)
   stroke(255/*,frameCount*3*/);
   branch(100, angle, 6, fc);
-  //if(frameCount >= 20) {
-   // noLoop();
+  if(frameCount >= 100) {
+    noLoop();
   //  saveCanvas('myCanvas', 'png');
-  //}
+  }
   depth*=.75;
-  fc+=2;
+  fc+=4;
 }
 function branch(length, angle, w, f) {
   if(w)
