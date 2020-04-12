cb9af42d83346738a8b050ab17eaa377af7f6d51
peterg4 <43735968+peterg4@users.noreply.github.com>
2020-04-11 23:42:15 -0400

    smoothly growing tree

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -3,8 +3,9 @@ p5.disableFriendlyErrors = true; // disables FES
 function setup() {
   var can = createCanvas(600, 600);
   can.parent('sketch');
-  frameRate(15);
+  frameRate(60);
 }
+let fc = 0;
 let depth = 100;
 function draw() {
   
@@ -13,30 +14,35 @@ function draw() {
   clear();
   translate(300,height)
   stroke(255/*,frameCount*3*/);
-  branch(100, angle, 6);
-  if(frameCount >= 20) {
-    noLoop();
- //   saveCanvas('myCanvas', 'png');
-  }
+  branch(100, angle, 6, fc);
+  //if(frameCount >= 20) {
+   // noLoop();
+  //  saveCanvas('myCanvas', 'png');
+  //}
   depth*=.75;
+  fc+=2;
 }
-function branch(length, angle, w) {
+function branch(length, angle, w, f) {
   if(w)
     strokeWeight(w);
+  if(f < length) {
+    line(0,0,0,-f);
+    return;
+  }
   line(0,0,0,-length);
   //drawLine(0,0,0,-length); //make a function to animate the line drawing process
   translate(0,-length);
   if(length > depth && length > 4) {
     if(w > 1)
       w--;
+    f-=length;
     push();
     rotate(angle);
-    branch(length*.75,angle*.9,w);
+    branch(length*.75,angle*.9,w,f);
     pop();
     push();
     rotate(-angle);
-    branch(length*.75,angle*.9,w);
-
+    branch(length*.75,angle*.9,w,f);
     pop();
   } 
 }
