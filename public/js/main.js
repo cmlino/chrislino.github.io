d947fa09665df8d8ebe732a3e08afcbc5c748ee2
peterg4 <43735968+peterg4@users.noreply.github.com>
2020-04-12 16:35:40 -0400

    card animation on scroll

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
+AOS.init();
+
 var app = angular.module("home", []);
 
-// Here is the Javascript for our controller which we linked (scoped) to the body tag
 app.controller("home-control", ['$scope','$http',function($scope, $http) {
 	$scope.view = 0;
 	$scope.one = 'active';
