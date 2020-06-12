66cf4ee602a7b942c7939c0da04129206b356ba4
peterg4 <43735968+peterg4@users.noreply.github.com>
2020-06-12 19:07:14 -0400

    mobile css

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -1,7 +1,7 @@
 p5.disableFriendlyErrors = true; // disables FES
 
 function setup() {
-  var can = createCanvas(600, 600);
+  var can = createCanvas(windowWidth-20, windowHeight);
   can.parent('sketch');
   frameRate(30);
 }
@@ -12,9 +12,10 @@ function draw() {
   let angle = PI/6;
   background(34);
   clear();
-  translate(300,height)
+  translate(width/2,height/1.6)
   stroke(255/*,frameCount*3*/);
-  branch(100, angle, 6, fc);
+
+  branch(min(floor(windowHeight*.13),100), angle, 6, fc);
   if(frameCount >= 97) {
     noLoop();
   //  saveCanvas('myCanvas', 'png');
@@ -22,6 +23,12 @@ function draw() {
   depth*=.75;
   fc+=4;
 }
+
+function windowResized() {
+  resizeCanvas(windowWidth-20, windowHeight);
+}
+
+
 function branch(length, angle, w, f) {
   if(w)
     strokeWeight(w);
@@ -37,11 +44,11 @@ function branch(length, angle, w, f) {
     f-=length;
     push();
     rotate(angle);
-    branch(length*.75,angle*.9,w,f);
+    branch(floor(length*.75),angle*.9,w,f);
     pop();
     push();
     rotate(-angle);
-    branch(length*.75,angle*.9,w,f);
+    branch(floor(length*.75),angle*.9,w,f);
     pop();
   } 
 }
