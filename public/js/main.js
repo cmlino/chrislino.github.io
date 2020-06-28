71401210e165ab4c009d5f7d2d2f9883a2561a06
peterg4 <43735968+peterg4@users.noreply.github.com>
2020-06-28 17:32:05 -0400

    tooltips for all icons

--- public/js/main.js
+++ public/js/main.js
@@ -6,3 +6,45 @@ tippy('.spotify', {
 	content: "Spotify Web API",
 	allowHTML: true,
 });
+
+tippy('.angularjs', {
+	content: 'Global content',
+	trigger: 'mouseenter focus',
+	content: "AngularJS",
+	allowHTML: true,
+});
+
+tippy('.bootstrap', {
+	content: 'Global content',
+	trigger: 'mouseenter focus',
+	content: "Bootstrap",
+	allowHTML: true,
+});
+
+tippy('.node', {
+	content: 'Global content',
+	trigger: 'mouseenter focus',
+	content: "Node.js",
+	allowHTML: true,
+});
+
+tippy('.mongo', {
+	content: 'Global content',
+	trigger: 'mouseenter focus',
+	content: "MongoDB",
+	allowHTML: true,
+});
+
+tippy('.react', {
+	content: 'Global content',
+	trigger: 'mouseenter focus',
+	content: "React.js",
+	allowHTML: true,
+});
+
+tippy('.tensorflow', {
+	content: 'Global content',
+	trigger: 'mouseenter focus',
+	content: "TensorFlow",
+	allowHTML: true,
+});
