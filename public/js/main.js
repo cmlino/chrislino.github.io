30f0d8c535d611f5e8c75af2c096c652fbc5d0e4
peterg4 <43735968+peterg4@users.noreply.github.com>
2020-06-28 17:24:24 -0400

    removed angularJS bloat

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,1 @@
 AOS.init();
-
-var app = angular.module("home", []);
-
-app.controller("home-control", ['$scope','$http',function($scope, $http) {
-	$scope.view = 0;
-	$scope.one = 'active';
-	$scope.items = [['public/css/images/jukebox.png','','JukeBox',"https://github.com/peterg4"]];
-}]);
