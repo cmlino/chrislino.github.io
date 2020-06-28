94408a8bc35ecf2f295fc3148d0c362b46ba65bb
peterg4 <43735968+peterg4@users.noreply.github.com>
2020-06-28 17:51:50 -0400

    tooltips for clickable icons

--- public/js/main.js
+++ public/js/main.js
@@ -6,30 +6,14 @@ tippy.setDefaultProps({
 	trigger: 'mouseenter focus'
 });
 
-tippy('.spotify', {
-	content: "Spotify Web API"
-});
-
-tippy('.angularjs', {
-	content: "AngularJS"
-});
-
-tippy('.bootstrap', {
-	content: "Bootstrap"
-});
-
-tippy('.node', {
-	content: "Node.js"
-});
-
-tippy('.mongo', {
-	content: "MongoDB"
-});
-
-tippy('.react', {
-	content: "React.js"
-});
-
-tippy('.tensorflow', {
-	content: "TensorFlow"
-});
+tippy('.spotify', {content: "Spotify Web API"});
+tippy('.angularjs', {content: "AngularJS"});
+tippy('.bootstrap', {content: "Bootstrap"});
+tippy('.node', {content: "Node.js"});
+tippy('.mongo', {content: "MongoDB"});
+tippy('.react', {content: "React.js"});
+tippy('.tensorflow', {content: "TensorFlow"});
+tippy('.fa-github', {content: "Github"});
+tippy('.fa-linkedin', {content: "Linkedin"});
+tippy('.fa-envelope', {content: "gpeterson707@gmail.com"});
+tippy('.fa-file', {content: "Resume"});
