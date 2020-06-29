a796088348c9ad3a9c743561a241788f0374d5de
peterg4 <43735968+peterg4@users.noreply.github.com>
2020-06-29 10:04:32 -0400

    material-ui icon

--- public/js/main.js
+++ public/js/main.js
@@ -13,6 +13,7 @@ tippy('.node', {content: "Node.js"});
 tippy('.mongo', {content: "MongoDB"});
 tippy('.react', {content: "React.js"});
 tippy('.tensorflow', {content: "TensorFlow"});
+tippy('.materialui', {content: "Material-UI"})
 tippy('.fa-github', {delay: 250, content: "Github"});
 tippy('.fa-linkedin', {delay: 250, content: "LinkedIn"});
 tippy('.fa-envelope', {delay: 250, content: "gpeterson707@gmail.com"});
